08dd67f536a03d3976c78828c4bb476b7586b570
joachimharms <joachimharms77@gmail.com>
2017-08-17 00:45:10 +0200

    Finish version 1.0 - done

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -14,16 +14,17 @@ import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+import static android.R.id.message;
+
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -31,20 +32,20 @@ public class MainActivity extends AppCompatActivity
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view)
-    {
-        displayPrice(quantity * 5);
+    public void submitOrder(View view) {
+        int price = quantity * 5;
+        String ans = "Total: $";
+        String priceMessage = ans + price + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     // This method is called when the increment button is clicked.
-    public void increment(View view)
-    {
+    public void increment(View view) {
         quantity = quantity + 1;
         display(quantity);
     }
 
-    public void decrement(View view)
-    {
+    public void decrement(View view) {
         quantity = quantity - 1;
         display(quantity);
     }
@@ -52,8 +53,7 @@ public class MainActivity extends AppCompatActivity
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number)
-    {
+    private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -61,9 +61,18 @@ public class MainActivity extends AppCompatActivity
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number)
-    {
+    private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
 }
