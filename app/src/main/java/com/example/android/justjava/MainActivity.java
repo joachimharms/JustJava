55c9c9611ba19dbb4a3a189fc2be7a0b10eedc7b
joachimharms <joachimharms77@gmail.com>
2017-08-26 23:09:40 +0200

    Add checkbox - done

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -19,12 +19,14 @@ import static android.R.id.message;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     int quantity = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -32,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
+    public void submitOrder(View view)
+    {
         int price = calculatePrice();
         displayMessage(orderSummary(price));
     }
@@ -42,24 +45,27 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param quantity is the number of cups of coffee ordered
      */
-    private int calculatePrice() {
+    private int calculatePrice()
+    {
         int price = quantity * 5;
         return price;
     }
 
-    private String orderSummary(int priceOfOrder){
-    String ans = "Name: Joachim Harms\nQuantity: " + quantity + "\nTotal $: " + calculatePrice() + " \nThank you!";
-    return  ans;
+    private String orderSummary(int priceOfOrder)
+    {
+        String ans = "Name: Joachim Harms\nQuantity: " + quantity + "\nTotal: $" + calculatePrice() + " \nThank you!";
+        return ans;
     }
 
-
     // This method is called when the increment button is clicked.
-    public void increment(View view) {
+    public void increment(View view)
+    {
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
-    public void decrement(View view) {
+    public void decrement(View view)
+    {
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
@@ -67,7 +73,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int numberLol) {
+    private void displayQuantity(int numberLol)
+    {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + numberLol);
     }
@@ -75,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
+    private void displayPrice(int number)
+    {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
@@ -84,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
+    private void displayMessage(String message)
+    {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
