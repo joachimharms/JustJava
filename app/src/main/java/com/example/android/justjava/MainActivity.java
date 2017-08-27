d605d80564a4b99b2dadf31d839b05bde3dba762
joachimharms <joachimharms77@gmail.com>
2017-08-27 22:20:18 +0200

    Add userinput validation - done

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,6 +7,11 @@
 
 package com.example.android.justjava;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +19,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -27,7 +33,7 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 public class MainActivity extends AppCompatActivity
 {
 
-    int quantity = 0;
+    int quantity = 99;
     String name = "";
 
     @Override
@@ -42,10 +48,17 @@ public class MainActivity extends AppCompatActivity
      */
     public void submitOrder(View view)
     {
-        EditText editName = (EditText) findViewById(R.id.edit_name_view);
-        name = editName.getText().toString();
-        int price = calculatePrice();
-        displayMessage(orderSummary(price, name));
+//        EditText editName = (EditText) findViewById(R.id.edit_name_view);
+//        name = editName.getText().toString();
+//        int price = calculatePrice();
+//        displayMessage(orderSummary(price, name));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:47.6, -122.3"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+
+    }
     }
 
     /**
@@ -55,7 +68,16 @@ public class MainActivity extends AppCompatActivity
      */
     private int calculatePrice()
     {
-        int price = quantity * 5;
+        int price = 0;
+        if (checkTopping())
+        {
+            price = quantity;
+        }
+        if (checkChocolate())
+        {
+            price += quantity * 2;
+        }
+        price += quantity * 5;
         return price;
     }
 
@@ -90,13 +112,26 @@ public class MainActivity extends AppCompatActivity
     // This method is called when the increment button is clicked.
     public void increment(View view)
     {
-        quantity = quantity + 1;
+
+
+        if (quantity == 100)
+        {
+            Toast.makeText(this, "Quantity can't be more than 100!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity += 1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view)
     {
-        quantity = quantity - 1;
+
+        if (quantity == 1)
+        {
+            Toast.makeText(this, "Quantity can't be less than 1!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity -= 1;
         displayQuantity(quantity);
     }
 
---
+++ app/src/test/java/com/example/android/justjava/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.android.justjava;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joachim on 27.08.2017.
+ */
+public class MainActivityTest
+{
+
+
+}
